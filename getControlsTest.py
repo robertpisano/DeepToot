f3f38bc441af755a4555d84f2db2a12789214a45
mgiglia92 <michael.a.giglia@gmail.com>
2020-02-23 22:14:45 -0500

    added GEKKO

--- getControlsTest.py
+++
@@ -1,7 +0,0 @@
-from ParserLibrary import ReplayConverter
-import carball
-
-json_file = carball.decompile_replay("D:/Documents/RL Replays/69DA1AB64F613BE0CD5F3A94EDD7BC24.replay",
-                    output_path = "D:/Documents/RL Replays",
-                    overwrite=True)
-self.game.initialize(loaded_json = json_file)
