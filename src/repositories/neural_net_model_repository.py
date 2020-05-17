c921f3484018054a6401b7e4f68dc9efc3b7b775
unknown <robert.j.pisano92@gmail.com>
2020-05-16 21:59:26 -0400

    adding a neural net model repository, without an implementation, used to abstract the retrieval of models from saved files/saving new models

---
+++ src/repositories/neural_net_model_repository.py
@@ -0,0 +1,3 @@
+# neural net models (for now) will be saved as files in our codebase
+# the retrieval of these models is something that will be based on a given strategy - which requires logic for finding a file based on a strategy
+# NB: we also may wind up needing to use a database or other storage faacility
