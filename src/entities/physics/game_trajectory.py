b293f268062618c671c185627188c43bbcf3237c
unknown <robert.j.pisano92@gmail.com>
2020-05-16 21:55:52 -0400

    adding a game trajectory which compiles trajectories from each actor in a game

---
+++ src/entities/physics/game_trajectory.py
@@ -0,0 +1,10 @@
+from DeepToot.src.entities.physics.trajectory import Trajectory
+class GameTrajectory():
+    OPPONENT_TRAJECTORY = None
+    BOT_TRAJECTORY = None
+    BALL_TRAJECTORY = None
+
+    def __init__(self, opp_trajectory:Trajectory, bot_trajectory:Trajectory, ball_trajectory:Trajectory):
+        OPPONENT_TRAJECTORY = opp_trajectory
+        BOT_TRAJECTORY = bot_trajectory
+        BALL_TRAJECTORY = ball_trajectory
