35237e1a7e99844a205ff3e0702c46ac630b1710
unknown <robertpisano@yahoo.com>
2020-07-29 20:12:41 -0400

    human bot.py

--- RLBOT/human/human_bot.py
+++ RLBOT/human/human_bot.py
@@ -1,19 +1,60 @@
 from rlbot.agents.base_agent import BaseAgent
 from controller_input import controller
-from .RLBOT.srcNeuralNetworkDataGenerator import NeuralNetworkManager, DataGeneratorManager
+
+import pandas as pd
 
 class Agent(BaseAgent):
+
+
+
     def initialize_agent(self):
         # This runs once before the bot starts up
         # self.controller_state = SimpleControllerState()
         # self.nn_manager = NeuralNetworkManager()
         # self.sc = ScenarioCreator()
         # self.sc.hardcoded_load()
+        col = ["location_x", "location_y", "location_z",
+            "velocity_x", "velocity_y", "velocity_z", 
+            "rotation_roll", "rotation_pitch", "rotation_yaw", 
+            "angular_velocity_x", "angular_velocity_y", "angular_velocity_z",
+            "controller_throttle", "controller_steer", "controller_boost",
+            "ping_flag"]
+        self.dataframe = pd.DataFrame([], columns = col)
+        self.ping_flag = 0
 
-        self.dataManager = DataGeneratorManager(120) # Send in how long we want to save data for 120 frames seems reasonable for testing
 
     def get_output(self, game_tick_packet):
-        if(self.dataManager.started and not self.dataManager.finished): 
-            self.dataManager.append(game_tick_packet) # Append data, and will check if complete
+        p = game_tick_packet.game_cars[self.index].physics
+        controller_output = controller.get_output()  
 
-        return controller.get_output()
+        if(controller_output.throttle > 0):
+            self.ping_flag = 1
+        else:
+            self.ping_flag = 0
+
+        data = [p.location.x, p.location.y, p.location.z, 
+                p.velocity.x, p.velocity.y, p.velocity.z, 
+                p.rotation.roll, p.rotation.pitch, p.rotation.yaw, 
+                p.angular_velocity.x, p.angular_velocity.y, p.angular_velocity.z,
+                controller_output.throttle, controller_output.steer, controller_output.boost, 
+                self.ping_flag]
+
+        a_series = pd.Series(data, index=self.dataframe.columns)
+        self.dataframe = self.dataframe.append(a_series, ignore_index=True)
+  
+        if(game_tick_packet.game_info.seconds_elapsed > 180):
+            data_path = os.path.dirname(DeepToot.__file__) + "\SavedData\stupid.csv\\"
+            self.dataframe.to_csv(data_path)
+
+        return controller_output
+    
+    def _get_file_path(self):
+            #model name to use for file name
+        model_type = self._model_type()
+        #create model directory if it doesn't exist
+        if not os.path.exists(self.MODELS_PATH): os.mkdir(self.MODELS_PATH)
+        #create file name
+        return self.MODELS_PATH + model_type
+    
+if __name__ == "__main__":
+    a = Agent([],[],[])
