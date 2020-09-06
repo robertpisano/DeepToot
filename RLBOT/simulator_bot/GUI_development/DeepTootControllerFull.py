3cb5c33549c2267559d02a4311aba89f9d18cf06
mgiglia92 <michael.a.giglia@gmail.com>
2020-09-06 00:04:32 -0400

    deleted unnecessary functions, added and edited comments

--- RLBOT/simulator_bot/GUI_development/DeepTootControllerFull.py
+++ RLBOT/simulator_bot/GUI_development/DeepTootControllerFull.py
@@ -10,8 +10,8 @@ from PyQt5.QtWidgets import QTableWidget, QTableWidgetItem
 
 
 class Ui_DeepTootControllerFull(Ui_DeepTootController):
-    """This extension takes the .ui converted to .py file class and extends it to 
-    implement all the connections and signals to make the GUI work properly.
+    """This extension inherits the .ui converted to .py file class and extends it to 
+    implement all the connections and signals to complete GUI functionality.
     
 
     Args:
@@ -20,15 +20,19 @@ class Ui_DeepTootControllerFull(Ui_DeepTootController):
     def set_event_handling(self):
         self.pushButton_execute.clicked.connect(gui_functions.print_hello)
         
-        # Set Driving COntroller table update signals
+        # Set Driving COntroller combobox update signals
         self.comboBox_drivingControllerType.currentIndexChanged.connect(lambda: self.populate_table(ControllerFactory.create(str(self.comboBox_drivingControllerType.currentText())).params, self.tableWidget_drivingParams))
         self.comboBox_drivingControllerType.currentIndexChanged.connect(lambda: self.populate_table(ControllerFactory.create(str(self.comboBox_drivingControllerType.currentText())).miscOptions, self.tableWidget_drivingMiscOptions))
         
-        # Set aerial controller table update signals
-        self.comboBox_aerialControllerType.currentIndexChanged.connect(lambda: self.populate_table(ControllerFactory.create(str(self.comboBox_aerialControllerType.currentText())).params, self.tableWidget_aerialParams))
-        self.comboBox_aerialControllerType.currentIndexChanged.connect(lambda: self.populate_table(ControllerFactory.create(str(self.comboBox_aerialControllerType.currentText())).miscOptions, self.tableWidget_aerialMiscOptions))
+        # Set aerial controller combobox update signals
+        self.comboBox_aerialControllerType.currentIndexChanged.connect(
+            lambda: self.populate_table(ControllerFactory.create(str(self.comboBox_aerialControllerType.currentText())).params, self.tableWidget_aerialParams)
+            )
+        self.comboBox_aerialControllerType.currentIndexChanged.connect(
+            lambda: self.populate_table(ControllerFactory.create(str(self.comboBox_aerialControllerType.currentText())).miscOptions, self.tableWidget_aerialMiscOptions)
+            )
         
-        # Set brain presets table update signals
+        # Set brain presets table combobox signals
         self.comboBox_brainPresets.currentIndexChanged.connect(
             lambda: self.populate_table(BrainFactory.create(str(self.comboBox_brainPresets.currentText())).params, self.tableWidget_brainParams)
         )
@@ -37,26 +41,47 @@ class Ui_DeepTootControllerFull(Ui_DeepTootController):
         )
 
     def populate_combo_boxes(self):
-        """Populate combo boxes with available classes
+        """Populate combo boxes with available classes from factories
         """        
+        # Driving Controller
         for i in ControllerFactory.controllerList:
             self.comboBox_drivingControllerType.addItem(i)
         
+        # Aerial Controller
         for i in ControllerFactory.controllerList:
             self.comboBox_aerialControllerType.addItem(i)
         
+        # Brain Presets
         for i in BrainFactory.brainList:
             self.comboBox_brainPresets.addItem(i)
 
+        # Initial COnditions
+        # TODO: popultae initial condition combobox
+
     def populate_table(self, optionList, table: QTableWidget):
+        """Take in list of options and table to place options on. Set items on
+        QWidgetTable passed in as well
+
+        Args:
+            optionList ([dict]): dictionary of parameter names and values
+            table (QTableWidget): table to display paramter names and values on
+        """        
+
+        # Set table to have 2 columns
         table.setColumnCount(2)
+        # Set table to have as many rows as there are options
         table.setRowCount(len(optionList.keys()))
 
+        #Disable sorting
         table.setSortingEnabled(False)
+
+        # remove headers
         table.horizontalHeader().setVisible(False)
         table.verticalHeader().setVisible(False)
 
         for row, key in enumerate(optionList):
+            # Set parameter name in first column
             table.setItem(row, 0, QTableWidgetItem(key))
+            # Set parameter value in second column
             table.setItem(row, 1, QTableWidgetItem(str(optionList[key])))
         
--- RLBOT/simulator_bot/GUI_development/gui_functions.py
+++ RLBOT/simulator_bot/GUI_development/gui_functions.py
@@ -2,15 +2,3 @@ from DeepToot.RLBOT.simulator_bot.GUI_development.meta_data_objects.MetaDataObje
 
 def print_hello():
     print("hello")
-
-def populate_params_table(obj: MetaDataObject):
-    if(obj is not None):
-        return obj.params
-    
-    pass
-
-def populate_misc_table(obj: MetaDataObject):
-    if(obj is not None):
-        return obj.miscOptions
-    
-    pass
