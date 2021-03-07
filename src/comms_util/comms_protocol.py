df0ab97eed72081f15b2dfcede91f4d58c16c516
mgiglia92 <michael.a.giglia@gmail.com>
2021-03-07 17:56:02 -0500

    Started a tcp/ip comms protocol and helper classes to act as an intermediate between the gui and the bot (client and server). Next need to update system manager to use these helper classes.

---
+++ src/comms_util/comms_protocol.py
@@ -0,0 +1,46 @@
+import pickle
+
+class CommsProtocol:
+    types = {"execute": "execute", "update": "update", "none": "None"}
+    som = "%%%" # Start of message key
+    sod = "$$$" # Start of data key
+    eom = "###" # End of message key
+    SERVER = '127.0.0.1'
+    PORT = 5050
+    ADDR = (SERVER, PORT)
+    FORMAT = 'utf-8'
+
+class Message:
+    def __init__(self, type = "None", data = None):
+        self.type = type
+        self.data = data
+
+class Decoder:
+    @staticmethod
+    def decode(msg: str):
+        """Convert the message string into it's message type and data parts
+
+        Args:
+            msg (Message): [description]
+        """        
+        # Split by the som, sod, eod
+        type, data = msg.split(bytes(CommsProtocol.sod, 'utf-8'))
+        type = str(type).replace(CommsProtocol.som, '') # Remove som
+        data = str(data).replace(CommsProtocol.eom, '') # Remove eom
+        return Message(type, data)
+
+class Encoder:
+    @staticmethod
+    def encode(type, data):
+        """Encode the message into bytes using som, sod, eod from CommsProtocol
+
+        Args:
+            type ([type]): [description]
+            data ([type]): [description]
+        """        
+        string = bytes(CommsProtocol.som, 'utf-8')  # Add start of msg (som) 
+        string += bytes(type, 'utf-8')              # Add type string
+        string += bytes(CommsProtocol.sod, 'utf-8') # Add start of data (sod)
+        string += pickle.dumps(data)                # Add data
+        string += bytes(CommsProtocol.eom, 'utf-8') # Add end of msg (eom)
+        return string
